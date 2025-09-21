0cb52e676acf20b5be80f9cba2b2ea2410173505
Alessandro Dias <alessandrodbatista@gmail.com>
2025-09-21 19:59:05 -0300

    add cancancan

--- library_backend/app/controllers/application_controller.rb
+++ library_backend/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  include Authentication
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: "forbidden" }, status: :forbidden
+  end
 end
